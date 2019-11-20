651ff9da006aab46b43c38bd7bc1e4f3ad11ce2e
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-11-20 10:04:52 -0500

    Added new models

--- lstm_ui_REST_API/api/models.py
+++ lstm_ui_REST_API/api/models.py
@@ -9,28 +9,51 @@ class EventLog(models.Model):
     max_activities_per_trace = models.FloatField(default=0)
     mean_duration = models.CharField(max_length=100, default="0 days")
     max_duration = models.CharField(max_length=100, default="0 days")
-    sf = models.CharField(max_length=10, default="simple")
-    tv = models.CharField(max_length=10, default="stedy")
 
     def __str__(self):
         return self.name
 
 class RunningCase(models.Model):
-    name = models.IntegerField()
-    prefix = models.CharField(max_length=250)
-    suffix = models.CharField(max_length=250)
+    prefix_size = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.prefix
+        return self.prefix_size
+
+class Activity(models.Model):
+    activity_number = models.IntegerField(default=0)
+    activity_name = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.activity_name
+
+# One To Many Table to do match with running case id and activities
+# running_case_id
+# activity_number
+
+# Event Log, Running Case
+# event_log_id
+# running_case_id
 
 class Role(models.Model):
-    name = models.CharField(max_length=250)
+    role_id = models.IntegerField(default=0)
+    role_name = models.CharField(max_length=100, default="")
 
     def __str__(self):
-        return self.name
+        return self.role_name
 
 class Time(models.Model):
-    name = models.CharField(max_length=250)
+    min = models.IntegerField(default=0)
+    max = models.IntegerField(default=0)
+    mean = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.name
+        return self.mean
+
+# One to Many Activity Role Table
+# activity_number
+# role_id
+
+# Activity Time Table
+# One to one
+# Weak entity
+# Embedded table
--- lstm_ui_REST_API/api/serializers.py
+++ lstm_ui_REST_API/api/serializers.py
@@ -4,19 +4,19 @@ from .models import EventLog, RunningCase, Role, Time
 class EventLogSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = EventLog
-        fields = ['id', 'url', 'name', 'number_of_traces', 'number_of_events', 'number_of_activities', 'avg_activities_per_trace', 'max_activities_per_trace', 'mean_duration', 'max_duration', 'sf', 'tv']
+        fields = ['id', 'url', 'name', 'number_of_traces', 'number_of_events', 'number_of_activities', 'avg_activities_per_trace', 'max_activities_per_trace', 'mean_duration', 'max_duration']
 
 class RunningCaseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = RunningCase
-        fields = ['id', 'name', 'prefix', 'suffix']
+        fields = ['id', 'prefix_size']
 
 class RoleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Role
-        fields = ['name']
+        fields = ['role_id', 'role_name']
 
 class TimeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Time
-        fields = ['name']
+        fields = ['min', 'max', 'mean']
