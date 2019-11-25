fc40a988adf3389c989db70b64128f279d9c1d62
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-11-25 17:22:07 -0500

    Added ActivityViewSet

--- lstm_ui_REST_API/api/views.py
+++ lstm_ui_REST_API/api/views.py
@@ -1,7 +1,19 @@
-from rest_framework import viewsets
-from .models import EventLog, RunningCase, Role, Time
-from .serializers import EventLogSerializer, RunningCaseSerializer, RoleSerializer, TimeSerializer
+#------------------------------------------------------------------------------
+# LSTM UI REST API
+# viewss.py
+# This file has classes that represent viewsets
+#------------------------------------------------------------------------------
 
+#------------------------------------------------------------------------------
+# Imports
+#------------------------------------------------------------------------------
+from rest_framework import viewsets
+from .models import EventLog, RunningCase, Activity, Role, Time
+from .serializers import EventLogSerializer, RunningCaseSerializer, ActivitySerializer, RoleSerializer, TimeSerializer
+
+#------------------------------------------------------------------------------
+# Classes that represent viewsets
+#------------------------------------------------------------------------------
 class EventLogViewSet(viewsets.ModelViewSet):
     '''
     API endpoint that allows event logs to be viewed or edited
@@ -15,6 +27,13 @@ class RunningCaseViewSet(viewsets.ModelViewSet):
     '''
     queryset = RunningCase.objects.all()
     serializer_class = RunningCaseSerializer
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    '''
+    API endpoint that allows activities to be viewed or edited
+    '''
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
 
 class RoleViewSet(viewsets.ModelViewSet):
     '''
