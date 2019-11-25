f19393c910807a7dfe8172a3a9a79876d0e978ce
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-11-25 17:13:30 -0500

    Improving serializers

--- lstm_ui_REST_API/api/serializers.py
+++ lstm_ui_REST_API/api/serializers.py
@@ -1,6 +1,18 @@
-from rest_framework import serializers
-from .models import EventLog, RunningCase, Role, Time
+#------------------------------------------------------------------------------
+# LSTM UI REST API
+# serializers.py
+# This file has classes that represent serializers
+#------------------------------------------------------------------------------
 
+#------------------------------------------------------------------------------
+# Imports
+#------------------------------------------------------------------------------
+from rest_framework import serializers
+from .models import EventLog, RunningCase, Activity, Role, Time
+
+#------------------------------------------------------------------------------
+# Classes that represent serializers
+#------------------------------------------------------------------------------
 class EventLogSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = EventLog
@@ -9,14 +21,19 @@ class EventLogSerializer(serializers.HyperlinkedModelSerializer):
 class RunningCaseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = RunningCase
-        fields = ['id', 'prefix_size']
+        fields = ['id', 'url', 'prefix_size', 'event_log']
+
+class ActivitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Activity
+        fields = ['id', 'url', 'activity_number', 'activity_name', 'running_case']
 
 class RoleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Role
-        fields = ['role_id', 'role_name']
+        fields = ['id', 'url', 'role_id', 'role_name', 'activity']
 
 class TimeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Time
-        fields = ['min', 'max', 'mean']
+        fields = ['id', 'url', 'min', 'max', 'mean', 'activity']
