09d0e86d3fb5ba09adec51adc6fb1ca132c3b07a
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-11-25 17:31:48 -0500

    Added new router register

--- lstm_ui_REST_API/lstm_ui_REST_API/urls.py
+++ lstm_ui_REST_API/lstm_ui_REST_API/urls.py
@@ -1,29 +1,31 @@
-"""lstm_ui_REST_API URL Configuration
+#------------------------------------------------------------------------------
+# LSTM UI REST API
+# urls.py
+# This file configures the app's urls
+#------------------------------------------------------------------------------
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+#------------------------------------------------------------------------------
+# Imports
+#------------------------------------------------------------------------------
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from api import views
 
+#------------------------------------------------------------------------------
+# Router
+#------------------------------------------------------------------------------
 router = routers.DefaultRouter()
 router.register('event_logs', views.EventLogViewSet)
 router.register('running_cases', views.RunningCaseViewSet)
+router.register('activities', views.ActivityViewSet)
 router.register('roles', views.RoleViewSet)
 router.register('times', views.TimeViewSet)
 
+
+#------------------------------------------------------------------------------
+# Url patterns
+#------------------------------------------------------------------------------
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls))
